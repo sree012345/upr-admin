9d359aa9dd1ef92804ee9ea489427b8ce62866d0
RG-Srimathi <60177424+RG-Srimathi@users.noreply.github.com>
2020-08-14 19:05:13 +1300

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,12 +11,25 @@ import { SerialNumber } from '../models/serialNumber-model';
 })
 export class LoginComponent implements OnInit {
   formData=new SerialNumber();
+  message="Your email or password is wrong please check..!"
+  name:boolean=true;
   constructor(public service:AdminloginService) { }
 
   ngOnInit(): void {
   }
   onSubmit(form: NgForm){
+    
     this.service.adminlogin(form.value).subscribe((res) => {
+      var status = res["response_code"];
+      console.log(status)
+      if(status!=200){
+         this.name=false;
+        console.log(status)
+      this.message=res["response_message"]
+      console.log(this.message)
+      }else{
+        
+      }
       console.log(res);
     })
   }
