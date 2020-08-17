6fe9c5e979a7115f414c3bdcb6e492a210ed0358
RG-Srimathi <60177424+RG-Srimathi@users.noreply.github.com>
2020-08-18 04:41:46 +1300

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SerialNumbersComponent } from './serial-numbers/serial-numbers.component';
 import { ProductComponent } from './product/product.component';
+import { ImportloadComponent } from './importload/importload.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   {path:'login',component:LoginComponent},
   {path:'serialNumber',component:SerialNumbersComponent},
-  {path:'productDetails',component:ProductComponent}
+  {path:'productDetails',component:ProductComponent},
+  {path:'load',component:ImportloadComponent}
 ];
 
 @NgModule({
---
+++ src/app/importload/importload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-importload',
+  templateUrl: './importload.component.html',
+  styleUrls: ['./importload.component.css']
+})
+export class ImportloadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
