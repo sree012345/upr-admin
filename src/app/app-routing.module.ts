8b104157affdc65bab2f495248e4bf8be460fb18
sonali <sonali@revolutiongamesindia.com>
2020-09-02 21:17:39 +0530

    content management section ui and api implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { AuthenticScanComponent } from './authentic-scan/authentic-scan.componen
 import { RecallComponent } from './recall/recall.component';
 import { ServiceBulletinComponent } from './service-bulletin/service-bulletin.component';
 import { MainComponentComponent } from './main-component/main-component.component';
+import {ContentManagementComponent} from './content-management/content-management.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
@@ -18,7 +19,9 @@ const routes: Routes = [
   {path:'authentic',component:AuthenticScanComponent},
   {path:'recall',component:RecallComponent},
   {path:'serviceBulletin',component:ServiceBulletinComponent},
-  {path:'main',component:MainComponentComponent}
+  {path:'main',component:MainComponentComponent},
+  {path:'contentManagement',component:ContentManagementComponent}
+
 ];
 
 @NgModule({
---
+++ src/app/models/contentManagement-model.ts
@@ -0,0 +1,9 @@
+export class addContentManagement{
+    document_id:number;
+    product_id:number;
+    product_name:any;
+    document_or_link_name:string;
+    begin_date:any;
+    end_date:any;
+    URL:string;
+}
