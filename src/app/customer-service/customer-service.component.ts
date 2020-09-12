b9528440d640ee6e747aa884ab83216a578378c5
RG-Lijojoy <54926951+RG-Lijojoy@users.noreply.github.com>
2020-09-12 09:40:50 +0530

    Customer Service section api completed

--- src/app/customer-service/customer-service.component.ts
+++ src/app/customer-service/customer-service.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CustomerServiceService} from '../services/customer-service.service';
-import {customerService} from '../models/customerService-model';
+import {customerService, ProductInfo} from '../models/customerService-model';
 import { NgForm } from '@angular/forms';
 import { adminAdduser } from '../models/adminAdduser-model';
 
@@ -17,13 +17,24 @@ export class CustomerServiceComponent implements OnInit {
   productdetails:any;
   productrecalldetails:any;
   productbulletindetails:any;
+  companyName:string;
+  companyId:number;  
+  SectionsDisplay:boolean=false;
+  productInfoDetails:ProductInfo=new ProductInfo();
   adminUserDetails:adminAdduser;  
   logedin=localStorage.getItem('loggedinAdminUser');
+  productinformationdetails: any;
+  infoMessage: string;
+  recallMessage: string;
+  bulletinMessage: string;
   constructor(public service:CustomerServiceService) { }
 
   ngOnInit(): void {
+    this.SectionsDisplay=false;
     this.adminUserDetails=JSON.parse(this.logedin|| '{}');
     this.adminUserDetails.company_id= this.adminUserDetails.company_id;
+    this.companyId= this.adminUserDetails.company_id;
+    this.companyName=this.adminUserDetails.company_name;
       this.service.get_productlist(this.adminUserDetails.company_id).subscribe(data => {
       this.customerlist = (data["response_body"]["products_details"]);
     });
@@ -31,7 +42,7 @@ export class CustomerServiceComponent implements OnInit {
 
   addCustomer(form1: NgForm) {
     this.validAddCustomer=false;
- 
+    this.SectionsDisplay=false;
     if (form1.value.company_id == "" || form1.value.company_id == undefined) {
       this.invalidAddCustomer= true;
       this.message = "Please select company.";
@@ -49,17 +60,42 @@ export class CustomerServiceComponent implements OnInit {
       console.log("working response");
       this.invalidAddCustomer= false;
       this.service.addcustomerservice(form1.value).subscribe(data => {
+        console.log(data)
         var status = data["response_code"];
         if (status == 200) {
           this.validAddCustomer = true;
-          this.message = data["response_message"];
+        this.productInfoDetails=(data ["response_body"]["product_details"]);
+        this.productrecalldetails=(data ["response_body"]["product_recall_details"])
+        this. productbulletindetails=(data ["response_body"]["product_builtin_details"])
+        this. productinformationdetails=(data ["response_body"]["product_information"])
+        if(this.productinformationdetails.length==0)
+        {
+          this.infoMessage="There are currently no information for this product"
+        }
+        else{
+          this.infoMessage="The following important documents and links have been provided by"+this.productInfoDetails.company_name;
+        }
+        if(this.productrecalldetails.length==0)
+        {
+          this.recallMessage="There are currently no recalls for this product"
+        }
+        else{
+          this.recallMessage="There are " +this.productrecalldetails.length+ " different recalls issued on this product,however the serial number provided is not among the ones that have been recalled";
+        }
+        if(this.productbulletindetails.length==0)
+        {
+          this.bulletinMessage="There are currently no service bulletins for this product."
+        }
+        else{
+          this.bulletinMessage="There are " +this.productbulletindetails.length+ " different service bullitin issued on this product";
+        }
+        this.SectionsDisplay=true;  
+        this.message = data["response_message"];
           this.service.filter('Register click');
           this.resetForm();
         }
 
-        this.productdetails=(data ["response_body"]["product_details"]);
-        this.productrecalldetails=(data ["response_body"]["product_recall_details"])
-        this. productbulletindetails=(data ["response_body"]["product_recall_details"])
+        
       })
     }
   }
--- src/app/models/customerService-model.ts
+++ src/app/models/customerService-model.ts
@@ -1,5 +1,33 @@
-export class customerService{
-    company_id:number;
-    product_id:number;
-    serial_number:string;
+export class customerService {
+    company_id: number;
+    product_id: number;
+    serial_number: string;
+}
+
+export class ProductInfo {
+    product_name:string;
+    authentic_registration_id: 1;
+    mobile_user_id: 2;
+    serial_number_id: 1;
+    product_id: 1;
+    first_name: string;
+    last_name: string;
+    email: string;
+    address: string;
+    city: string;
+    state_province: string;
+    postal_code: string;
+    country: string;
+    phone: string;
+    location_lat: string;
+    location_long:string;
+    is_registered:boolean;
+    comments: string;
+    date_scanned: any;
+    date_registered:any;
+    is_active: boolean;
+    company_name: string;
+    company_website: string;
+    serial_number: string;
+    is_authentic_product:boolean;
 }
